2972c4d509137cd04068daee3b94cd2b13d01d6a
Yohan Liyanage <yohan@computer.org>
2011-11-26 21:19:00 +0530

    JBoss 4.2.3 JMX Sensor Test - Sandbox Initial Commit

---
+++ source/sandbox/sensors-jboss423-jmx/src/main/java/net/insightas/sandbox/sensors/jboss423/jmx/JBoss423JMXTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2011 Yohan Liyanage.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package net.insightas.sandbox.sensors.jboss423.jmx;
+
+import java.util.Properties;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.naming.CommunicationException;
+import javax.naming.InitialContext;
+
+import net.insightas.sandbox.sensors.api.ServerInfo;
+import net.insightas.sandbox.sensors.exception.SensorConnectionFailureException;
+import net.insightas.sandbox.sensors.jboss423.jmx.JBossJMXConstants.JNDIConstants;
+import net.insightas.sandbox.sensors.util.JMXUtil;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * This is a test class for JBoss 423 JMX Sensor Testing.
+ * 
+ * @author Yohan Liyanage (yohan at computer dot org)
+ * 
+ */
+public class JBoss423JMXTest {
+    
+    /**
+     * 
+     */
+    private static final String JBOSS_JMX_ADAPTER = "jmx/invoker/RMIAdaptor";
+
+    private static final Log LOG = LogFactory.getLog(JBoss423JMXTest.class);
+    
+    private static final String JBOSS_JNDI_URL = "jnp://127.0.0.1:1099";
+    
+    public static void main(String[] args) throws Exception {
+        
+        LOG.info("Test");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Preparing JNDI Connection...");
+        }
+        
+        // Prepare for JNDI
+        Properties props = new Properties();
+        props.put(JNDIConstants.JAVA_NAMING_FACTORY_INITIAL, JNDIConstants.JBOSS_JAVA_NAMING_FACTORY_INITIAL_VALUE);
+        props.put(JNDIConstants.JAVA_NAMING_PROVIDER_URL, JBOSS_JNDI_URL);
+        props.put(JNDIConstants.JAVA_NAMING_FACTORY_URL_PKGS, JNDIConstants.JBOSS_JAVA_NAMING_FACTORY_URL_PKGS_VALUE);
+        
+        InitialContext ctx = new InitialContext(props);
+        
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Connecting to MBean Server on JBoss Server at " + JBOSS_JNDI_URL + "...");
+        }
+        
+        // Connect to MBean Server
+        MBeanServerConnection server;
+        
+        try {
+            server = (MBeanServerConnection) ctx.lookup(JBOSS_JMX_ADAPTER);
+        } catch (CommunicationException e) {
+            LOG.warn("Unable to establish connection to MBean Server [" + JBOSS_JMX_ADAPTER + "]  on " + JBOSS_JNDI_URL, e);
+            throw new SensorConnectionFailureException("Cannot connect to MBean Server [" + JBOSS_JMX_ADAPTER + "] on " + JBOSS_JNDI_URL, e);
+        }
+        
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Connection established to remote MBean server at " + JBOSS_JNDI_URL);
+        }
+        
+        // ServerInfo MBean
+        final ObjectName mbServerInfo = new ObjectName("jboss.system:type=ServerInfo");
+        
+        // Gather Server Information
+        ServerInfo serverInfo = new ServerInfo();
+        
+        serverInfo.setHostAddress(JMXUtil.getStringAttribute(server, mbServerInfo, "HostAddress"));
+        serverInfo.setHostName(JMXUtil.getStringAttribute(server, mbServerInfo, "HostName"));
+        serverInfo.setJavaVendor(JMXUtil.getStringAttribute(server, mbServerInfo, "JavaVendor"));
+        serverInfo.setJavaVersion(JMXUtil.getStringAttribute(server, mbServerInfo, "JavaVersion"));
+        serverInfo.setJvmName(JMXUtil.getStringAttribute(server, mbServerInfo, "JavaVMName"));
+        serverInfo.setOsArchitecture(JMXUtil.getStringAttribute(server, mbServerInfo, "OSArch"));
+        serverInfo.setOsName(JMXUtil.getStringAttribute(server, mbServerInfo, "OSName"));
+        serverInfo.setOsVersion(JMXUtil.getStringAttribute(server, mbServerInfo, "OSVersion"));
+        serverInfo.setCpuCount(JMXUtil.getStringAttribute(server, mbServerInfo, "AvailableProcessors"));
+        
+        System.out.println(serverInfo);
+//        // Gather Heap Information
+//        String strMem = (server.invoke(new ObjectName("jboss.system:type=ServerInfo"), "listMemoryPools", new Object[] { Boolean.FALSE },
+//                new String[] { "boolean" })).toString();
+//        
+//        System.out.println(strMem);
+        
+    }
+    
+}
---
+++ source/sandbox/sensors-jboss423-jmx/src/main/java/net/insightas/sandbox/sensors/jboss423/jmx/JBossJMXConstants.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2011 Yohan Liyanage.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package net.insightas.sandbox.sensors.jboss423.jmx;
+
+
+/**
+ * JBoss JMX Constants.
+ * 
+ * @author Yohan Liyanage (yohan at computer dot org)
+ *
+ */
+public final class JBossJMXConstants {
+
+    /**
+     * JNDI Constants.
+     * 
+     * @author Yohan Liyanage (yohan at computer dot org)
+     *
+     */
+    public static final class JNDIConstants {
+        
+        /**
+         * JNDI Property Key for JBoss JNDI Factory. 
+         */
+        public static final String JAVA_NAMING_FACTORY_INITIAL = "java.naming.factory.initial";
+        
+        /**
+         * JNDI Property Value for JBoss JNDI Factory.
+         */
+        public static final String JBOSS_JAVA_NAMING_FACTORY_INITIAL_VALUE = "org.jnp.interfaces.NamingContextFactory";
+        
+        /**
+         *  JNDI Property Key for JBoss JNDI Factory URL Packages. 
+         */
+        public static final String JAVA_NAMING_FACTORY_URL_PKGS = "java.naming.factory.url.pkgs";
+
+        
+        /**
+         * JNDI Property Value for JBoss JNDI Factory URL Packages.
+         */
+        public static final String JBOSS_JAVA_NAMING_FACTORY_URL_PKGS_VALUE = "org.jnp.interfaces";
+        
+        /**
+         * JNDI Property Key for JBoss JNDI Provider URL.
+         */
+        public static final String JAVA_NAMING_PROVIDER_URL = "java.naming.provider.url";
+
+        /**
+         * Private Constructor - No External Instantiation. 
+         */
+        private JNDIConstants() {
+            super();
+        }
+        
+    }
+    
+    
+    /**
+     * Private Constructor - No External Instantiation.
+     */
+    private JBossJMXConstants() {
+        super();
+    }
+    
+    
+}
