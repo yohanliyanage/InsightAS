caad889352caf4d7cfde73dee2cf19d2d8f0c03d
Yohan Liyanage <yohan@computer.org>
2011-11-27 00:31:37 +0530

    Added Memory (Heap) extraction logic

---
+++ source/sandbox/sensors-jboss423-jmx/src/main/java/net/insightas/sandbox/sensors/jboss423/jmx/HeapStatDTO.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2011 Yohan Liyanage.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package net.insightas.sandbox.sensors.jboss423.jmx;
+
+/**
+ * @author Yohan Liyanage (yohan at computer dot org)
+ * 
+ */
+public class HeapStatDTO {
+    
+    private Long peakUsed;
+    private Long peakCommitted;
+    private Long peakMax;
+    
+    private Long currentUsed;
+    private Long currentCommitted;
+    private Long currentMax;
+    
+    /**
+     * @return the peakUsed
+     */
+    public Long getPeakUsed() {
+        return peakUsed;
+    }
+    
+    /**
+     * @param peakUsed the peakUsed to set
+     */
+    public void setPeakUsed(Long peakUsed) {
+        this.peakUsed = peakUsed;
+    }
+    
+    /**
+     * @return the peakCommitted
+     */
+    public Long getPeakCommitted() {
+        return peakCommitted;
+    }
+    
+    /**
+     * @param peakCommitted the peakCommitted to set
+     */
+    public void setPeakCommitted(Long peakCommitted) {
+        this.peakCommitted = peakCommitted;
+    }
+    
+    /**
+     * @return the peakMax
+     */
+    public Long getPeakMax() {
+        return peakMax;
+    }
+    
+    /**
+     * @param peakMax the peakMax to set
+     */
+    public void setPeakMax(Long peakMax) {
+        this.peakMax = peakMax;
+    }
+    
+    /**
+     * @return the currentUsed
+     */
+    public Long getCurrentUsed() {
+        return currentUsed;
+    }
+    
+    /**
+     * @param currentUsed the currentUsed to set
+     */
+    public void setCurrentUsed(Long currentUsed) {
+        this.currentUsed = currentUsed;
+    }
+    
+    /**
+     * @return the currentCommitted
+     */
+    public Long getCurrentCommitted() {
+        return currentCommitted;
+    }
+    
+    /**
+     * @param currentCommitted the currentCommitted to set
+     */
+    public void setCurrentCommitted(Long currentCommitted) {
+        this.currentCommitted = currentCommitted;
+    }
+    
+    /**
+     * @return the currentMax
+     */
+    public Long getCurrentMax() {
+        return currentMax;
+    }
+    
+    /**
+     * @param currentMax the currentMax to set
+     */
+    public void setCurrentMax(Long currentMax) {
+        this.currentMax = currentMax;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "HeapStatDTO [peakUsed=" + peakUsed + ", peakCommitted=" + peakCommitted + ", peakMax=" + peakMax + ", currentUsed="
+                + currentUsed + ", currentCommitted=" + currentCommitted + ", currentMax=" + currentMax + "]";
+    }
+    
+    
+}
--- source/sandbox/sensors-jboss423-jmx/src/main/java/net/insightas/sandbox/sensors/jboss423/jmx/JBoss423JMXTest.java
+++ source/sandbox/sensors-jboss423-jmx/src/main/java/net/insightas/sandbox/sensors/jboss423/jmx/JBoss423JMXTest.java
@@ -13,21 +13,37 @@
 
 package net.insightas.sandbox.sensors.jboss423.jmx;
 
+import java.io.StringReader;
 import java.util.Properties;
 
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.naming.CommunicationException;
 import javax.naming.InitialContext;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import net.insightas.sandbox.sensors.api.JavaMemoryInfo;
 import net.insightas.sandbox.sensors.api.ServerInfo;
 import net.insightas.sandbox.sensors.exception.SensorConnectionFailureException;
+import net.insightas.sandbox.sensors.exception.SensorFailureException;
 import net.insightas.sandbox.sensors.jboss423.jmx.JBossJMXConstants.JNDIConstants;
 import net.insightas.sandbox.sensors.util.JMXUtil;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.ccil.cowan.tagsoup.Parser;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 /**
  * This is a test class for JBoss 423 JMX Sensor Testing.
@@ -95,7 +111,7 @@ public class JBoss423JMXTest {
         serverInfo.setOsVersion(JMXUtil.getStringAttribute(server, mbServerInfo, "OSVersion"));
         serverInfo.setCpuCount(JMXUtil.getStringAttribute(server, mbServerInfo, "AvailableProcessors"));
         
-        System.out.println(serverInfo);
+        LOG.info("Server Info : " + serverInfo);
         
         // // Gather Heap Information
         String strHeapXML = (JMXUtil.invokeOperation(server, mbServerInfo, "listMemoryPools", new Object[] { Boolean.FALSE },
@@ -106,6 +122,8 @@ public class JBoss423JMXTest {
         javaMemInfo.setJvmTotalUsedMemory(JMXUtil.getLongAttribute(server, mbServerInfo, "TotalMemory"));
         
         extractHeapInfo(strHeapXML, javaMemInfo);
+        
+        LOG.info("Java Memory Info : " + javaMemInfo);
     }
     
     /**
@@ -114,8 +132,118 @@ public class JBoss423JMXTest {
      * @param strHeapXML
      * @param javaMemInfo
      */
-    private static void extractHeapInfo(String strHeapXML, JavaMemoryInfo javaMemInfo) {
-        System.out.println(strHeapXML);
+    private static void extractHeapInfo(String strHeapXML, JavaMemoryInfo javaMemInfo) throws SensorFailureException {
+        
+        // Replace <br> with semi-colon for easier string processing later.
+        strHeapXML = strHeapXML.replaceAll("<br>", " ; ");
+        
+        // Parse DOM Document
+        DOMResult result;
+        
+        try {
+            // Parse my (HTML) URL into a well-formed document
+            XMLReader reader = new Parser();
+            reader.setFeature(Parser.namespacesFeature, false);
+            reader.setFeature(Parser.namespacePrefixesFeature, false);
+            
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            
+            result = new DOMResult();
+            
+            transformer.transform(new SAXSource(reader, new InputSource(new StringReader(strHeapXML))), result);
+            
+        } catch (SAXException e) {
+            LOG.error("SAX Exception when parsing Sensor XML - " + e.getMessage());
+            throw new SensorFailureException("SAX Exception when parsing Sensor XML", e);
+        } catch (TransformerException e) {
+            LOG.error("XML Transformation Exception when parsing Sensor XML - " + e.getMessage());
+            throw new SensorFailureException("XML Transformation Exception when parsing Sensor XML", e);
+        }
+        
+        try {
+            
+            // Generate XPath for each segment
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            
+            XPathExpression xpCodeCache = xpath.compile("/html/body/blockquote/blockquote[1]");
+            XPathExpression xpEden = xpath.compile("/html/body/blockquote/blockquote[2]");
+            XPathExpression xpSurvivor = xpath.compile("/html/body/blockquote/blockquote[3]");
+            XPathExpression xpOldGen = xpath.compile("/html/body/blockquote/blockquote[4]");
+            XPathExpression xpPermGen = xpath.compile("/html/body/blockquote/blockquote[5]");
+            
+            HeapStatDTO codeCacheStats = extractStatFromString((String) xpCodeCache.evaluate(result.getNode(), XPathConstants.STRING));
+            HeapStatDTO edenStats = extractStatFromString((String) xpEden.evaluate(result.getNode(), XPathConstants.STRING));
+            HeapStatDTO survivorStats = extractStatFromString((String) xpSurvivor.evaluate(result.getNode(), XPathConstants.STRING));
+            HeapStatDTO oldGenStats = extractStatFromString((String) xpOldGen.evaluate(result.getNode(), XPathConstants.STRING));
+            HeapStatDTO permGenStats = extractStatFromString((String) xpPermGen.evaluate(result.getNode(), XPathConstants.STRING));
+            
+            javaMemInfo.setCodeCacheCurrentCommitted(codeCacheStats.getCurrentCommitted());
+            javaMemInfo.setCodeCacheCurrentMax(codeCacheStats.getCurrentMax());
+            javaMemInfo.setCodeCacheCurrentUsed(codeCacheStats.getCurrentUsed());
+            javaMemInfo.setCodeCachePeakCommitted(codeCacheStats.getPeakCommitted());
+            javaMemInfo.setCodeCachePeakMax(codeCacheStats.getPeakMax());
+            javaMemInfo.setCodeCachePeakUsed(codeCacheStats.getPeakUsed());
+            
+            javaMemInfo.setEdenCurrentCommitted(edenStats.getCurrentCommitted());
+            javaMemInfo.setEdenCurrentMax(edenStats.getCurrentMax());
+            javaMemInfo.setEdenCurrentUsed(edenStats.getCurrentUsed());
+            javaMemInfo.setEdenPeakCommitted(edenStats.getPeakCommitted());
+            javaMemInfo.setEdenPeakMax(edenStats.getPeakMax());
+            javaMemInfo.setEdenPeakUsed(edenStats.getPeakUsed());
+            
+            javaMemInfo.setSurvivorCurrentCommitted(survivorStats.getCurrentCommitted());
+            javaMemInfo.setSurvivorCurrentMax(survivorStats.getCurrentMax());
+            javaMemInfo.setSurvivorCurrentUsed(survivorStats.getCurrentUsed());
+            javaMemInfo.setSurvivorPeakCommitted(survivorStats.getPeakCommitted());
+            javaMemInfo.setSurvivorPeakMax(survivorStats.getPeakMax());
+            javaMemInfo.setSurvivorPeakUsed(survivorStats.getPeakUsed());
+            
+            javaMemInfo.setOldGenCurrentCommitted(oldGenStats.getCurrentCommitted());
+            javaMemInfo.setOldGenCurrentMax(oldGenStats.getCurrentMax());
+            javaMemInfo.setOldGenCurrentUsed(oldGenStats.getCurrentUsed());
+            javaMemInfo.setOldGenPeakCommitted(oldGenStats.getPeakCommitted());
+            javaMemInfo.setOldGenPeakMax(oldGenStats.getPeakMax());
+            javaMemInfo.setOldGenPeakUsed(oldGenStats.getPeakUsed());
+            
+            javaMemInfo.setPermGenCurrentCommitted(permGenStats.getCurrentCommitted());
+            javaMemInfo.setPermGenCurrentMax(permGenStats.getCurrentMax());
+            javaMemInfo.setPermGenCurrentUsed(permGenStats.getCurrentUsed());
+            javaMemInfo.setPermGenPeakCommitted(permGenStats.getPeakCommitted());
+            javaMemInfo.setPermGenPeakMax(permGenStats.getPeakMax());
+            javaMemInfo.setPermGenPeakUsed(permGenStats.getPeakUsed());
+            
+        } catch (XPathExpressionException e) {
+            LOG.error("XPathExpressionException when parsing Sensor XML - " + e.getMessage());
+            throw new SensorFailureException("XPathExpressionException when parsing Sensor XML", e);
+        }
+        
+    }
+
+    /**
+     * @param strCodeCache
+     * @return
+     */
+    private static HeapStatDTO extractStatFromString(String strCodeCache) {
+
+        HeapStatDTO stat = new HeapStatDTO();
+        
+        // Replace comma with colon for easier segmenting
+        strCodeCache = strCodeCache.replaceAll(",", ":");
+        
+        // Split segments
+        String[] segments = strCodeCache.split(";");
+        String[] peakSegments = segments[0].split(":");
+        String[] currentSgements = segments[1].split(":");
+        
+        stat.setPeakUsed(Long.valueOf(peakSegments[4].trim()));
+        stat.setPeakCommitted(Long.valueOf(peakSegments[6].trim()));
+        stat.setPeakMax(Long.valueOf(peakSegments[8].trim()));
+        
+        stat.setCurrentUsed(Long.valueOf(currentSgements[4].trim()));
+        stat.setCurrentCommitted(Long.valueOf(currentSgements[6].trim()));
+        stat.setCurrentMax(Long.valueOf(currentSgements[8].trim()));
+        
+        return stat;
     }
     
 }
