85c5edb54fe89cc8d11f2a11be0dccd7c5ce7fc8
Yohan Liyanage <yohan@computer.org>
2011-12-10 20:02:28 +0530

    Added CPU Info Class

--- source/sandbox/sensor-api/src/main/java/net/insightas/sandbox/sensors/dto/CpuInfo.java
+++ source/sandbox/sensor-api/src/main/java/net/insightas/sandbox/sensors/dto/CpuInfo.java
@@ -13,13 +13,88 @@
 
 package net.insightas.sandbox.sensors.dto;
 
-
 /**
  * CPU Information DTO.
  * 
  * @author Yohan Liyanage (yohan at computer dot org)
- *
+ * 
  */
 public class CpuInfo {
+    
+    private String vendor;
+    private String model;
+    private int clockSpeedMHz;
+    private int cores;
+    private int cacheSizeKB;
+    
+    /**
+     * @return the vendor
+     */
+    public String getVendor() {
+        return vendor;
+    }
+    
+    /**
+     * @param vendor the vendor to set
+     */
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+    
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+    
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+    /**
+     * @return the clockSpeedMHz
+     */
+    public int getClockSpeedMHz() {
+        return clockSpeedMHz;
+    }
+    
+    /**
+     * @param clockSpeedMHz the clockSpeedMHz to set
+     */
+    public void setClockSpeedMHz(int clockSpeedMHz) {
+        this.clockSpeedMHz = clockSpeedMHz;
+    }
+    
+    /**
+     * @return the cores
+     */
+    public int getCores() {
+        return cores;
+    }
+    
+    /**
+     * @param cores the cores to set
+     */
+    public void setCores(int cores) {
+        this.cores = cores;
+    }
+    
+    /**
+     * @return the cacheSizeKB
+     */
+    public int getCacheSizeKB() {
+        return cacheSizeKB;
+    }
+    
+    /**
+     * @param cacheSizeKB the cacheSizeKB to set
+     */
+    public void setCacheSizeKB(int cacheSizeKB) {
+        this.cacheSizeKB = cacheSizeKB;
+    }
     
 }
